c54d51dc2f4318510e4ee94cd37bc9328912dc71
timiolarry <38512262+RotimiOlarry@users.noreply.github.com>
2023-01-03 17:25:17 -0600

    Solution to January 3 Leetcode

---
+++ jan3.py
@@ -0,0 +1,13 @@
+strs = ["cba","daf","ghi"]
+
+
+def whatif(word):
+    counter = 0
+    for i in range(len(strs[0])):
+         for j in range(1,len(strs)):
+            if strs[j-1][i] > strs[j][i]:
+                counter+= 1
+                break
+    return counter
+        
+print(whatif(strs))
